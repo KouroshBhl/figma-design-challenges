9c30f7291b7eba4589e5539381bbb8a07622c23f
Kourosh Baharloo <kourosh9bhl@gmail.com>
2023-01-13 16:09:55 +0330

    Implemented card and bg

--- src/challenges/SubscriptionSection/BackgroundCard.js
+++ src/challenges/SubscriptionSection/BackgroundCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const BackgroundCard = () => {
-  return <div>BackgroundCard</div>;
+  return <Wrapper></Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 90%;
+  height: 67.5rem;
+  background: #1e1d4e;
+  border-radius: 24px;
+  position: absolute;
+  top: -6%;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 export default BackgroundCard;
--- src/challenges/SubscriptionSection/SubscriptionSection.js
+++ src/challenges/SubscriptionSection/SubscriptionSection.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import BackgroundCard from './BackgroundCard';
 
 const SubscriptionSection = () => {
-  return <div>SubscriptionSection</div>;
+  return (
+    <Wrapper>
+      <div className='card-container'>
+        <Card></Card>
+        <BackgroundCard />
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  min-height: 92vh;
+  background-color: #f1f1f6;
+  margin: 0 auto;
+
+  .card-container {
+    position: relative;
+    width: 101rem;
+    height: 60rem;
+  }
+`;
+
+const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 2.4rem;
+  box-shadow: 30px 4px 84px rgba(22, 20, 96, 0.16);
+  position: relative;
+  z-index: 10;
+`;
 
 export default SubscriptionSection;
